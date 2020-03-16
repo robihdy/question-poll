2f93eef89e7c9b9be6b04897a9fb33f39356e46f
Robi Hidayat <robihdy@gmail.com>
2020-03-16 17:23:41 +0700

    connect server and database with prisma client

--- server/src/index.js
+++ server/src/index.js
@@ -1,58 +1,26 @@
 const { GraphQLServer } = require('graphql-yoga');
-
-let questions = [
-  {
-    id: 'q-0',
-    description: 'What is GraphQL?',
-    votes: 0
-  },
-  {
-    id: 'q-1',
-    description: 'What are the differences between GraphQL and REST?',
-    votes: 0
-  }
-];
-
-let idCount = questions.length;
+const { prisma } = require('./generated/prisma-client');
 
 const resolvers = {
   Query: {
     info: () => `This is the API of a Question Poll app`,
-    questions: () => questions,
-    question: (parent, args) =>
-      questions.find(question => question.id === args.id)
+    questions: (root, args, context, info) => {
+      return context.prisma.questions();
+    }
   },
   Mutation: {
-    ask: (parent, args) => {
-      const question = {
-        id: `q-${idCount++}`,
+    ask: (root, args, context) => {
+      return context.prisma.createQuestion({
         description: args.description,
         votes: 0
-      };
-      questions.push(question);
-      return question;
-    },
-    updateQuestion: (parent, args) => {
-      const idx = questions.findIndex(question => question.id === args.id);
-      if (idx === -1) {
-        return;
-      }
-      questions[idx].description = args.description;
-      return questions[idx];
-    },
-    deleteQuestion: (parent, args) => {
-      const idx = questions.findIndex(question => question.id === args.id);
-      console.log(idx);
-      if (idx === -1) {
-        return;
-      }
-      return questions.splice(idx, idx + 1)[0];
+      });
     }
   }
 };
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
-  resolvers
+  resolvers,
+  context: { prisma }
 });
 server.start(() => console.log(`Server is running on http://localhost:4000`));
