b1ef1fde72c836ab5792b1958988cf2473e24490
Robi Hidayat <robihdy@gmail.com>
2020-03-16 16:39:07 +0700

    add question schema

--- server/src/index.js
+++ server/src/index.js
@@ -7,34 +7,52 @@ let questions = [
     votes: 0
   },
   {
-    id: 'q-2',
+    id: 'q-1',
     description: 'What are the differences between GraphQL and REST?',
     votes: 0
   }
 ];
 
-const typeDefs = `
-type Query {
-  info: String!
-  questions: [Question!]!
-}
-
-type Question {
-  id: ID!
-  description: String!,
-  votes: Int!
-}
-`;
+let idCount = questions.length;
 
 const resolvers = {
   Query: {
     info: () => `This is the API of a Question Poll app`,
-    questions: () => questions
+    questions: () => questions,
+    question: (parent, args) =>
+      questions.find(question => question.id === args.id)
+  },
+  Mutation: {
+    ask: (parent, args) => {
+      const question = {
+        id: `q-${idCount++}`,
+        description: args.description,
+        votes: 0
+      };
+      questions.push(question);
+      return question;
+    },
+    updateQuestion: (parent, args) => {
+      const idx = questions.findIndex(question => question.id === args.id);
+      if (idx === -1) {
+        return;
+      }
+      questions[idx].description = args.description;
+      return questions[idx];
+    },
+    deleteQuestion: (parent, args) => {
+      const idx = questions.findIndex(question => question.id === args.id);
+      console.log(idx);
+      if (idx === -1) {
+        return;
+      }
+      return questions.splice(idx, idx + 1)[0];
+    }
   }
 };
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers
 });
 server.start(() => console.log(`Server is running on http://localhost:4000`));
